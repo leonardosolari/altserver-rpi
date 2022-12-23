0e68e7273fee110633f2930ad4d20396921e2545
Leonardo Solari <56320744+leonardosolari@users.noreply.github.com>
2022-12-23 18:25:08 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt install -y libavahi-compat-libdnssd-dev usbmuxd ninja-build ldc libplist-dev libimobiledevice-dev
+libgtk-3-0 dub 
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+rustup toolchain install stable
+
+rustup default stable
+
+sudo apt install libusbmuxd-dev build-essential pkg-config checkinstall git autoconf automake libtool-bin libplist-dev openssl
---
+++ run.sh
@@ -0,0 +1,4 @@
+sudo systemctl enable --now avahi-daemon.service
+sudo systemctl enable --now usbmuxd.service
+screen -S netmuxd -dm ./aarch64-linux-netmuxd --disable-unix --host 127.0.0.1
+screen -S altserver -dm bash -c "export USBMUXD_SOCKET_ADDRESS=127.0.0.1:27015; export ALTSERVER_ANISETTE_SERVER="http://127.0.0.1:6969"; ./AltServer-aarch64"
