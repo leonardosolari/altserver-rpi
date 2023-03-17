380e6c3b307033dd747667bd65783231b0c7d691
Leo2660 <leonardosolari67@gmail.com>
2023-03-17 21:59:17 +0100

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-systemctl enable --now avahi-daemon.service
+/etc/init.d/docker avahi-daemon.service
 systemctl enable --now usbmuxd.service
 screen -S netmuxd -dm ./bin/aarch64-linux-netmuxd --disable-unix --host 127.0.0.1
 screen -S altserver -dm bash -c "export USBMUXD_SOCKET_ADDRESS=127.0.0.1:27015; export ALTSERVER_ANISETTE_SERVER="http://127.0.0.1:6969"; ./bin/AltServer-aarch64"
