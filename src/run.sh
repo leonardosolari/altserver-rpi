a7ec475cbe0fde9a44354817d353d69440405e60
Leo2660 <leonardosolari67@gmail.com>
2023-03-18 07:32:45 +0100

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-/etc/init.d/dbus start
-/etc/init.d/avahi-daemon start
-usbmuxd
-screen -S netmuxd -dm ./bin/aarch64-linux-netmuxd --disable-unix --host 127.0.0.1
-screen -S altserver -dm bash -c "export USBMUXD_SOCKET_ADDRESS=127.0.0.1:27015; export ALTSERVER_ANISETTE_SERVER="http://127.0.0.1:6969"; ./bin/AltServer-aarch64"
+sudo systemctl enable --now avahi-daemon.service
+sudo systemctl enable --now usbmuxd.service
+screen -S netmuxd -dm ./aarch64-linux-netmuxd --disable-unix --host 127.0.0.1
+screen -S altserver -dm bash -c "export USBMUXD_SOCKET_ADDRESS=127.0.0.1:27015; export ALTSERVER_ANISETTE_SERVER="http://127.0.0.1:6969"; ./AltServer-aarch64"
