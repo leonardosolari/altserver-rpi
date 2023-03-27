ed7635fa800c350c3b05366b1e04c6b56a734f44
Leo2660 <leonardosolari67@gmail.com>
2023-03-27 16:18:53 +0200

    Update start.sh

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -8,12 +8,12 @@ ps -ef | grep avahi
 
 
 echo "Starting usbmuxd..."
-/usr/sbin/usbmuxd --user usbmux --systemd
-ps -ef | grep usbmuxd
+#/usr/sbin/usbmuxd --user usbmux --systemd
+#ps -ef | grep usbmuxd
 
 
 echo "Starting netmuxd..."
-screen -S netmuxd -dm ./aarch64-linux-netmuxd --disable-unix --host 127.0.0.1
+./aarch64-linux-netmuxd --disable-unix --host 127.0.0.1
 
 echo "Starting altserver..."
 ./AltServer-aarch64
